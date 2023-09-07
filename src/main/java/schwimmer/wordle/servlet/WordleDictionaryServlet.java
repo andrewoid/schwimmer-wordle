6da9da1881b3eaa9f4d8e65a55f4f108a39ca4bd
Andrew Schwimmer <andrew.schwimmer@disneystreaming.com>
2023-09-07 11:07:28 -0400

    added WordleDictionaryServletTest

--- src/main/java/schwimmer/wordle/servlet/WordleDictionaryServlet.java
+++ src/main/java/schwimmer/wordle/servlet/WordleDictionaryServlet.java
@@ -14,8 +14,28 @@ import java.io.IOException;
  */
 public class WordleDictionaryServlet extends HttpServlet {
 
+<<<<<<< Updated upstream
     private WordleDictionary dictionary = new WordleDictionary();
     private Gson gson = new Gson();
+=======
+    private WordleDictionary dictionary;
+    private Gson gson;
+
+    /**
+     * This is used by Jetty
+     */
+    public WordleDictionaryServlet() {
+        this(new WordleDictionary(), new Gson());
+    }
+
+    /**
+     * This is used in tests
+     */
+    public WordleDictionaryServlet(WordleDictionary dictionary, Gson gson) {
+        this.dictionary = dictionary;
+        this.gson = gson;
+    }
+>>>>>>> Stashed changes
 
     @Override
     protected void doGet(
